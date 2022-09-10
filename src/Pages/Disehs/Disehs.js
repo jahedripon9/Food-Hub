818810874f459ca13d9a4843987a76fb5ebf3bd7
jahedripon9 <jahedahmed3170@gmail.com>
2022-09-10 19:03:57 +0600

    Deshes Section Updating

--- src/Pages/Disehs/Disehs.js
+++ src/Pages/Disehs/Disehs.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Disehs = () => {
     return (
-        <div>
+        <Container className='my-3'>
             <h2 className='display-5 fw-bold mt-5'>More then 20,000 Dishes <br /> To Order</h2>
             <p className='fw-semibold text-secondary'>Welcome to biggest Network of Food Ordering & Delivery</p>
-        </div>
+            <div class="row row-cols-1 row-cols-md-6 g-4 " >
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/HnD2X65/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SUSHI</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light :hover">
+                        <img   src="https://i.ibb.co/qj4kZHy/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PIZZA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/w0Ddm10/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">BURGERS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/D14r2xP/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">DESSERTS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/St8k2Dd/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SALAD</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/bKD100B/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PASTA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+
+        </Container>
+
     );
 };
 
--- src/Pages/HeroSection/HeroSection.js
+++ src/Pages/HeroSection/HeroSection.js
@@ -6,15 +6,15 @@ const HeroSection = () => {
         <div>
             <Container>
                 <Row >
-                    <Col className='text-start' xs={12} md={6}>
-                        <h1 className='display-3 fw-bolder text-sm-start my-5'>We Provide <br /> Delivery Within <br /> 30 Min</h1>
+                    <Col className='text-start' xs={12} md={5}>
+                        <h1 className='display-4 fw-bolder text-sm-start my-5'>We Provide <br /> Delivery Within <br /> 30 Min</h1>
 
                         <p className='text-sm-start text-mute'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia a et reiciendis veritatis eum corrupti rerum explicabo minus, nihil magnam suscipit sapiente facere? Praesentium, perferendis.</p>
                         <input type="text" className='rounded-5 p-2 px-4' placeholder='Zip Code' name="Zip Code" id=""></input>
                         <Button className='rounded-5 fw-semibold p-2 px-4'>Search</Button>
                     </Col>
-                    <Col xs={12} md={6}>
-                    
+                    <Col xs={12} md={7}>
+                    <img height='450px' width='100%'  className='mt-2' src="https://i.ibb.co/JyWd1h6/5227.jpg" alt="" />
                     </Col>
                 </Row>
             </Container>
