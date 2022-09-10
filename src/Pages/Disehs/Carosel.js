68c44a5e1c147891637dc9a28b17de8097611e8a
jahedripon9 <jahedahmed3170@gmail.com>
2022-09-10 19:33:23 +0600

    Deshes Section Updating

---
+++ src/Pages/Disehs/Carosel.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+
+const Carosel = () => {
+    return (
+        <Container>
+            <Carousel variant="dark">
+      <Carousel.Item>
+      <div class="row row-cols-1 row-cols-md-6 g-4 " >
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/HnD2X65/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SUSHI</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light :hover">
+                        <img   src="https://i.ibb.co/qj4kZHy/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PIZZA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/w0Ddm10/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">BURGERS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/D14r2xP/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">DESSERTS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/St8k2Dd/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SALAD</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/bKD100B/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PASTA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div class="row row-cols-1 row-cols-md-6 g-4 " >
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/HnD2X65/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SUSHI</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light :hover">
+                        <img   src="https://i.ibb.co/qj4kZHy/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PIZZA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/w0Ddm10/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">BURGERS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/D14r2xP/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">DESSERTS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/St8k2Dd/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SALAD</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/bKD100B/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PASTA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div class="row row-cols-1 row-cols-md-6 g-4 " >
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/HnD2X65/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SUSHI</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light :hover">
+                        <img   src="https://i.ibb.co/qj4kZHy/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PIZZA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/w0Ddm10/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">BURGERS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/D14r2xP/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">DESSERTS</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/St8k2Dd/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">SALAD</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card bg-light">
+                        <img   src="https://i.ibb.co/bKD100B/image.png" class="card-img-top p-3  w-auto center" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">PASTA</h5>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+      </Carousel.Item>
+    </Carousel>
+        </Container>
+    );
+};
+
+export default Carosel;
--- src/Pages/Disehs/Disehs.js
+++ src/Pages/Disehs/Disehs.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Carosel from './Carosel';
 
 const Disehs = () => {
     return (
         <Container className='my-3'>
             <h2 className='display-5 fw-bold mt-5'>More then 20,000 Dishes <br /> To Order</h2>
             <p className='fw-semibold text-secondary'>Welcome to biggest Network of Food Ordering & Delivery</p>
-            <div class="row row-cols-1 row-cols-md-6 g-4 " >
+            {/* <div class="row row-cols-1 row-cols-md-6 g-4 " >
                 <div class="col">
                     <div class="card bg-light">
                         <img   src="https://i.ibb.co/HnD2X65/image.png" class="card-img-top p-3  w-auto center" alt="..." />
@@ -63,7 +64,8 @@ const Disehs = () => {
                 </div>
                 
                 
-            </div>
+            </div> */}
+            <Carosel />
 
         </Container>
 
